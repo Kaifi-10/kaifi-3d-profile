f7f7413a159d2907f39a3c7830d2a2180469a016
Kaifi-10 <murghoobkaifi@gmail.com>
2025-05-01 19:07:23 +0530

    sociallinks update

--- src/constants/index.js
+++ src/constants/index.js
@@ -187,12 +187,12 @@ export const socialLinks = [
     {
         name: 'GitHub',
         iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
+        link: 'https://github.com/Kaifi-10',
     },
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+        link: 'https://www.linkedin.com/in/mdmurghoobkaifi/',
     }
 ];
 
